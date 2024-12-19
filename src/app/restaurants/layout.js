b2b401c6d0126a0ef8a6a95c945b1769f7093692
Игнатов Алексей <a.ignatov@vr-protect.ru>
2024-12-19 16:42:52 +0300

    refactor: сделать ShowCart компонентом-контейнером

--- src/app/restaurants/layout.js
+++ src/app/restaurants/layout.js
@@ -2,6 +2,7 @@ import ShowCart from "../../components/show-cart/show-cart.js";
 import RestaurantsTabsContainer from "../../components/restaurants-tabs-container/restaurants-tabs-container.jsx";
 import { Suspense } from "react";
 import Loader from "../../components/loader/loader.jsx";
+import Cart from "../../components/cart/cart.jsx";
 
 const LayoutRestaurantsPage = ({ children }) => {
   return (
@@ -10,7 +11,9 @@ const LayoutRestaurantsPage = ({ children }) => {
         <RestaurantsTabsContainer />
       </Suspense>
       <main>{children}</main>
-      <ShowCart />
+      <ShowCart>
+        <Cart />
+      </ShowCart>
     </>
   );
 };
